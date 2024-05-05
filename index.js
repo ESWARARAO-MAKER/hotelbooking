cdeb4b379714c09b91c355e7d33dd1cef1286c48
ESWARARAO-MAKER <balagaeswararao2002@gmail.com>
2024-05-05 11:33:30 +0530

    update port

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import cors from "cors";
 
 const app = express();
 dotenv.config();
+const port = process.env.PORT || 4000;
+
 
 const connect = async () => {
   try {
@@ -45,7 +47,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(8800, () => {
+app.listen(port, () => {
   connect();
   console.log("Connected to backend.");
 });
